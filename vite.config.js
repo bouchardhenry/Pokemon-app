f4be74485d3b76b4ab4c8c027fc1536e38c27535
bouchardhenry <bouchardhenry@gmail.com>
2025-11-25 19:40:23 +0100

    p

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Pokemon-app-2/',
+  base: '/pokemon-app-2/',
   plugins: [react()]
 })
